9c637a2b9a5e032cf7af8f8c8b95039fe6734b38
Ebenezer Ackon <ebenezer.ackon@viacom.com>
2018-03-02 02:17:42 -0500

    add Logs and more logic in mainactivity just for testing

--- app/src/main/java/jfyg/etherscan/helloetherescan/MainActivity.kt
+++ app/src/main/java/jfyg/etherscan/helloetherescan/MainActivity.kt
@@ -7,40 +7,50 @@ import android.view.Menu
 import android.view.MenuItem
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
-import jfyg.etherscan.helloetherescan.network.responses.BaseResponse
+import jfyg.etherscan.helloetherescan.ethereum.EthereumStat
+import jfyg.etherscan.helloetherescan.model.EtherPriceModel
+import jfyg.etherscan.helloetherescan.network.responses.MainResponse
 import jfyg.etherscan.helloetherescan.network.RestClient
 
 import kotlinx.android.synthetic.main.activity_main.*
-
+//TODO: clean up and write tests
 class MainActivity : AppCompatActivity() {
     private val TAG = javaClass.name!!
+
+    var etherPrice = EtherPriceModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        var testing = EthereumStat()
 
         fab.setOnClickListener {
-            queryPrice("1I7CRNU2QIU253UBPFVB5UV2C2PBDURAIY")
+            //testing.getTotalSupply()
+            //testing.getTotalSupplyInWei()
+            testing.getLastPriceInUsd()
+            testing.getLastPriceInBtc()
+            //queryStats("stats", "ethPrice")
         }
     }
 
-    private fun queryPrice(apiKey: String) {
+    fun queryStats(module: String, action: String) {
+        //pass apikey that will save in preferences or singleton or something.. or application class
         RestClient().getQuery()
-                .getEtherStats("stats", "ethprice", apiKey)
+                .getEtherStats(module, action, "1I7CRNU2QIU253UBPFVB5UV2C2PBDURAIYZ")
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe(this::handleResponse, this::handleError)
     }
 
-    private fun handleResponse(retrieveQuery: BaseResponse) {
-        Log.d(TAG, "status: " + retrieveQuery.status)
-        Log.d(TAG, "message: " + retrieveQuery.message)
-        Log.d(TAG, "Eth in Btc: " + retrieveQuery.etherPriceresult?.ethBtc)
-        Log.d(TAG, "Eth in Btc Timestamp: " + retrieveQuery.etherPriceresult?.ethBtcTimestamp)
-        Log.d(TAG, "Eth in Usd: " + retrieveQuery.etherPriceresult?.ethUsd)
-        Log.d(TAG, "Eth in Usd Timestamp: " + retrieveQuery.etherPriceresult?.ethUsdTimestamp)
-        //Log.d(TAG, "Eth supply: " + retrieveQuery.etherSupplyResult)
+    private fun handleResponse(retrieveQuery: MainResponse) {
+        etherPrice.status = retrieveQuery.status
+        etherPrice.message = retrieveQuery.message
+        etherPrice.ethBtc = retrieveQuery.etherPriceresult?.ethBtc
+        etherPrice.ethBtcTimestamp = retrieveQuery.etherPriceresult?.ethBtcTimestamp
+        etherPrice.ethUsd = retrieveQuery.etherPriceresult?.ethUsd
+        etherPrice.ethUsdTimestamp = retrieveQuery.etherPriceresult?.ethUsdTimestamp
     }
 
     private fun handleError(error: Throwable) {
@@ -62,4 +72,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
