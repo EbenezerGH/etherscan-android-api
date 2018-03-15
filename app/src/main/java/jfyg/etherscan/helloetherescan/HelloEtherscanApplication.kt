18e581f6c5806b6999859286c2c231a944f384c0
Ebenezer Ackon <ebenezer.ackon@viacom.com>
2018-03-15 15:29:12 -0400

    Add code to main to test etherscan module queries

--- app/src/main/java/jfyg/etherscan/helloetherescan/HelloEtherscanApplication.kt
+++ app/src/main/java/jfyg/etherscan/helloetherescan/HelloEtherscanApplication.kt
@@ -6,6 +6,8 @@ import jfyg.ApiKey
 class HelloEtherscanApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        //************************************************  Used To Test Library
         ApiKey.takeOff.setApiKey("1I7CRNU2QIU253UBPFVB5UV2C2PBDURAIYZ")
     }
 }
--- app/src/main/java/jfyg/etherscan/helloetherescan/MainActivity.kt
+++ app/src/main/java/jfyg/etherscan/helloetherescan/MainActivity.kt
@@ -3,21 +3,31 @@ package jfyg.etherscan.helloetherescan
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import jfyg.stat.EthereumStat
+import jfyg.stat.Stat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val stat = EthereumStat()
+    private val TAG = javaClass.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        //************************************************  Used To Test Library
+        val stat = Stat()
+
         fab.setOnClickListener {
-            Log.d("Ebenezer", stat.getLastPriceInBtc())
+
+            // Log.d(TAG, "${stat.NetworkInfo()}") //TODO #29
+            Log.d(TAG, "The current price of Ether in Btc:  ${stat.getLastPriceInBtc()}")
+            Log.d(TAG, "The current price of Ether in Usd:  ${stat.getLastPriceInUsd()}")
+            Log.d(TAG, "The total supply of Ether:  ${stat.getTotalSupply()}")
+            Log.d(TAG, "The total supply of Ether in Wei:  ${stat.getTotalSupplyInWei()}")
+
         }
     }
+
 
 }
