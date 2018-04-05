9c651228b4168edb95c0424377cfd8eae58293de
Ebenezer Ackon <ebenezer.ackon@viacom.com>
2018-04-05 16:37:15 -0400

    cleanup todos, restructure code

--- etherscanapi/src/main/java/jfyg/account/Account.kt
+++ etherscanapi/src/main/java/jfyg/account/Account.kt
@@ -1,67 +1,39 @@
 package jfyg.account
 
-import android.annotation.SuppressLint
 import jfyg.model.Balances
 import jfyg.model.Blocks
 import jfyg.model.Transactions
 import jfyg.queries.QueryMediator
 
 class Account : AccountContract {
-    private var query = QueryMediator()
 
-    override fun getBalance(account: String?): Double? {
-        query.accountBalance("account",
-                "balance",
-                account,
-                "latest")
+    private val query = QueryMediator()
+    private val genericNetworkQuery = query.accountBalance("account", "balance",
+            "0x82e4499D4b2A669831a3881d61BB24f7b620c61a", "latest")
 
-        return query.fetchAccountBalance()?.result?.toDouble()
+    override fun getBalance(account: String?): Double? = let {
+        query.accountBalance("account", "balance",
+                account, "latest").let { query.fetchAccountBalance()?.result?.toDouble() }
     }
 
-    override fun getMultiBalance(accounts: List<String>?): ArrayList<Balances>? {
-        query.accountMultiBalance("account",
-                "balance",
-                accounts?.get(0), //todo #34
-                "latest")
-
-        return query.fetchAccountMultiBalance()?.result
+    override fun getMultiBalance(accounts: List<String>?): ArrayList<Balances>? = let { //todo #34
+        query.accountMultiBalance("account", "balance",
+                accounts?.get(0), "latest").let { query.fetchAccountMultiBalance()?.result }
     }
 
-    override fun getBlocks(account: String?): ArrayList<Blocks>? {
-        query.accountBlock("account",
-                "getminedblocks",
-                account,
-                "blocks")
-
-        return query.fetchAccountBlock()?.result
+    override fun getBlocks(account: String?): ArrayList<Blocks>? = let { //todo #29
+        query.accountBlock("account", "getminedblocks",
+                account, "blocks").let { query.fetchAccountBlock()?.result }
     }
 
-    override fun getTransactions(account: String?): ArrayList<Transactions>? {
-        query.accountTransactions("account",
-                "txlist",
-                account,
-                "0",
-                "99999999",
-                "asc")
-        return query.fetchAccountTransaction()?.result
+    override fun getTransactions(account: String?): ArrayList<Transactions>? = let { //todo #29
+        query.accountTransactions("account", "txlist",
+                account, "0", "99999999",
+                "asc").let { query.fetchAccountTransaction()?.result }
     }
 
-    override fun getNetworkStatus(): String? {
-        query.accountBalance("account",
-                "balance",
-                "0x82e4499D4b2A669831a3881d61BB24f7b620c61a",
-                "latest")
+    override fun getNetworkStatus(): String? = genericNetworkQuery.let { query.fetchAccountBalance()?.status }
 
-        return query.fetchAccountBalance()?.status
-    }
-
-    override fun getNetworkMessage(): String? {
-        query.accountBalance("account",
-                "balance",
-                "0x82e4499D4b2A669831a3881d61BB24f7b620c61a",
-                "latest")
-
-        return query.fetchAccountBalance()?.message
-    }
+    override fun getNetworkMessage(): String? = genericNetworkQuery.let { query.fetchAccountBalance()?.message }
 
 }
